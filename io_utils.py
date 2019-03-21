6d7262fdfd98ffedbf34410627631b602bf5166c
Nicolay Rusnachenko <kolyarus@yandex.ru>
2019-03-21 11:50:29 +0300

    removed unused methods

--- io_utils.py
+++ io_utils.py
@@ -1,45 +1,13 @@
 #!/usr/bin/python
-import io
 import numpy as np
-from os import path, makedirs, mkdir
+from os import path, makedirs
 from os.path import dirname
-from gensim.models.word2vec import Word2Vec
 from core.processing.lemmatization.base import Stemmer
 from core.evaluation.statistic import FilesToCompare
 
 ignored_entity_values = [u"author", u"unknown"]
 TEST = 'test'
 TRAIN = 'train'
-
-
-def get_server_root():
-    return u'/home/nicolay/storage/disk/homes/nicolay/datasets/news'
-
-
-def read_prepositions(filepath):
-    prepositions = []
-    with io.open(filepath, 'r', encoding='utf-8') as f:
-        for line in f.readlines():
-            prepositions.append(line.strip())
-
-    return prepositions
-
-
-def read_lss(filepath):
-    words = []
-    with io.open(filepath, 'r', encoding='utf-8') as f:
-        for line in f.readlines():
-            words.append(line.lower().strip())
-
-    return words
-
-
-def read_feature_names():
-    features = []
-    with open(get_feature_names_filepath(), 'r') as f:
-        for l in f.readlines():
-            features.append(l.strip())
-    return features
 
 
 def train_indices():
@@ -81,33 +49,6 @@ def eval_root():
     return path.join(data_root(), u"Eval/")
 
 
-def test_root():
-    result = path.join(data_root(), u"Test/")
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
-def pretrained_root():
-    result = path.join(data_root(), u"Pretrained/")
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
-def load_w2v_model(filepath=path.join(data_root(), u"w2v/news_rusvectores2.bin.gz"), binary=True):
-    print u"Loading word2vec model '{}' ...".format(filepath)
-    w2v_model = Word2Vec.load_word2vec_format(filepath, binary=binary)
-    return w2v_model
-
-
-def graph_root():
-    result = path.join(data_root(), u"Graphs/")
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
 def get_collection_root():
     return path.join(data_root(), u"Collection/")
 
@@ -125,104 +66,9 @@ def get_opin_filepath(index, is_etalon, prefix=u'art', root=get_collection_root(
     return path.join(root, u"{}{}.opin{}.txt".format(prefix, index, '' if is_etalon else u'.result'))
 
 
-def get_constraint_opin_filepath(index, root=get_collection_root()):
-    return path.join(root, u"art{}.opin.graph.txt".format(index))
-
-
 def get_neutral_filepath(index, is_train, root=get_collection_root()):
     assert(isinstance(is_train, bool))
     return path.join(root, u"art{}.neut.{}.txt".format(index, TRAIN if is_train else TEST))
-
-
-def get_vectors_filepath(index, is_train, root=get_collection_root()):
-    assert(isinstance(is_train, bool))
-    return path.join(root, u"art{}.vectors.{}.txt".format(index, TRAIN if is_train else TEST))
-
-
-def eval_rfe_root():
-    result = path.join(eval_root(), u"rfe")
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
-def eval_sfm_root():
-    result = path.join(eval_root(), u"sfm")
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-def eval_sfm_cv_root(cv_count):
-    assert(isinstance(cv_count, int))
-    result = path.join(eval_root(), u"sfm_{}".format(cv_count))
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
-def eval_ec_root():
-    """ class elemination root
-    """
-    result = path.join(eval_root(), u"ce")
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
-def eval_univariate_root():
-    result = path.join(eval_root(), u"uv")
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
-def eval_default_root():
-    result = path.join(eval_root(), u"default")
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
-def eval_default_cv_root(cv_count):
-    assert(isinstance(cv_count, int))
-    result = path.join(eval_root(), u"default_cv_{}".format(cv_count))
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
-def eval_ensemble_cv_root(cv_count):
-    assert(isinstance(cv_count, int))
-    result = path.join(eval_root(), u"ensemble_cv_{}".format(cv_count))
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
-def eval_ensemble_root():
-    result = path.join(eval_root(), u"ensemble")
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
-def eval_baseline_root():
-    result = path.join(eval_root(), u"baseline")
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
-def eval_baseline_cv_root(cv_count):
-    assert(isinstance(cv_count, int))
-    result = path.join(eval_root(), u"baseline_cv_{}".format(cv_count))
-    if not path.exists(result):
-        makedirs(result)
-    return result
-
-
-def eval_features_filepath():
-    return path.join(eval_root(), u'features')
 
 
 def get_method_root(method_name):
@@ -232,39 +78,8 @@ def get_method_root(method_name):
     return result
 
 
-def get_vectors_list(is_train, indices=None, root=get_collection_root()):
-    if indices is None:
-        indices = train_indices() if is_train else test_indices()
-    return [get_vectors_filepath(i, is_train, root) for i in indices]
-
-
-def get_etalon_root():
-    return path.join(data_root(), u"Etalon/")
-
-
 def get_synonyms_filepath():
     return path.join(data_root(), u"synonyms.txt")
-
-
-def get_feature_names_filepath():
-    return path.join(data_root(), u"feature_names.txt")
-
-
-def get_log_csv_filepath(file_name):
-    return path.join(eval_root(), u'{}.csv'.format(file_name))
-
-
-def save_test_opinions(test_opinions, method_name, indices=test_indices()):
-    """
-    Save list of opinions
-    """
-    method_root = get_method_root(method_name)
-    if not path.exists(method_root):
-        mkdir(method_root)
-
-    for i, test_index in enumerate(indices):
-        # TODO. should guarantee that order the same as during reading operation.
-        test_opinions[i].save(get_opin_filepath(test_index, is_etalon=False, root=method_root))
 
 
 def create_files_to_compare_list(method_name, indices=test_indices(), root=get_collection_root()):
